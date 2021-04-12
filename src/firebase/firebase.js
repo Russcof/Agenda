925c8b5cc8605b1390d1779772ccf448ad15dabb
Russcof <60407248+Russcof@users.noreply.github.com>
2021-04-12 21:21:15 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCqZjBnmzFZuYj0V7grYlsVQ8ZFmjdje7g",
-    authDomain: "agenda-9d535.firebaseapp.com",
-    projectId: "agenda-9d535",
-    storageBucket: "agenda-9d535.appspot.com",
-    messagingSenderId: "1079859154982",
-    appId: "1:1079859154982:web:4c1a9318b74863c61ad6e7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   const fire = firebase.initializeApp(firebaseConfig);
